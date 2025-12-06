5c653e41769288fc277c7109a189f9a2721b5a90
LPalmer23 <109111183+LPalmer23@users.noreply.github.com>
2025-12-06 18:45:59 +1100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -86,8 +86,7 @@ app = FastAPI(
 origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://zyq-quantum-option-pricer.vercel.app",
-    "https://quantum-option-pricer-launch.vercel.app",
+    "https://quantum-option-pricer-launch-3fdf.vercel.app",
 ]
 
 
